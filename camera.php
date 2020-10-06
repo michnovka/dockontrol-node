907633ff3e3a1698664963549d029dea30a1a50f
Tomas <michnovka@gmail.com>
2020-10-06 19:06:01 +0200

    Added camera API

---
+++ camera.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @param string $stream_url
+ * @param string $stream_login
+ * @return bool|string
+ */
+function fetchCameraPicture($stream_url, $stream_login){
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, $stream_url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	if($stream_login) {
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
+		curl_setopt($ch, CURLOPT_USERPWD, $stream_login);
+	}
+
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	return curl_exec($ch);
+}
+
+$photo_data = fetchCameraPicture('http://'.$_GET['ip'].'/ISAPI/Streaming/channels/102/picture?videoResolutionWidth=1920&videoResolutionHeight=1080', 'admin:admin12345');
+
+header('Content-type: image/jpeg');
+echo $photo_data;
