b6c843926d49f1b6ec3621abc8ffb81b39ae6112
Tomas <michnovka@gmail.com>
2024-12-12 01:05:43 +0100

    Verify api2.php requests

--- www/public/api2.php
+++ www/public/api2.php
@@ -5,32 +5,53 @@ require __DIR__ . '/../vendor/autoload.php';
 use DockontrolNode\GPIO;
 use DockontrolNode\API;
 
-function APISignedError($message, $code): never
+function APISignedError(string $message, int $httpCode): never
 {
     $apiPrivKey = getenv('API_PRIVATE_KEY');
     API::performDockontrolNodeAPIResponse(
         $apiPrivKey,
         $_SERVER['HTTP_X_API_TIMESTAMP'],
         $_SERVER['HTTP_X_API_SIGNATURE'],
-        ['status' => 'error', 'code' => $code, 'message' => $message],
-        403
+        ['status' => 'error', 'message' => $message],
+        $httpCode
     );
 }
 
-function APIError($message, $code): never
+function APIError(string $message, int $httpCode): never
 {
     header('Content-type: application/json');
-    echo json_encode(['status' => 'error', 'code' => $code, 'message' => $message]);
+    http_response_code($httpCode);
+    echo json_encode(['status' => 'error', 'message' => $message]);
     exit;
 }
 
-$_SECRET = getenv('API_PRIVATE_KEY');
+$_API_PRIVATE_KEY = getenv('API_PRIVATE_KEY');
+$_API_PUBLIC_KEY = getenv('API_PUBLIC_KEY');
 
-if(empty($_SECRET)) {
-    APIError("No API private key configured.", 403);
+if(empty($_API_PRIVATE_KEY)) {
+    APIError("No API private key configured.", 500);
 }
 
-// TODO: verify the request
+if(empty($_API_PUBLIC_KEY)) {
+    APIError("No API public key configured.", 500);
+}
+
+if(empty($_SERVER['HTTP_X_API_KEY']) || empty($_SERVER['HTTP_X_API_TIMESTAMP']) || empty($_SERVER['HTTP_X_API_SIGNATURE'])) {
+    APIError("Required headers missing", 403);
+}
+
+if(!API::verifyDockontrolNodeAPIRequest(
+    $_API_PUBLIC_KEY,
+    $_API_PRIVATE_KEY,
+    $_SERVER['HTTP_X_API_KEY'],
+    $_SERVER['HTTP_X_API_TIMESTAMP'],
+    $_SERVER['HTTP_X_API_SIGNATURE'],
+    $_SERVER['REQUEST_METHOD'],
+    $_SERVER['PATH_INFO'],
+    file_get_contents('php://input'),
+)){
+    APIError("Unauthorized", 403);
+}
 
 $response = [];
 
@@ -46,7 +67,7 @@ switch($_POST['action'] ?? ''){
 }
 
 API::performDockontrolNodeAPIResponse(
-    $_SECRET,
+    $_API_PRIVATE_KEY,
     $_SERVER['HTTP_X_API_TIMESTAMP'],
     $_SERVER['HTTP_X_API_SIGNATURE'],
     $response
